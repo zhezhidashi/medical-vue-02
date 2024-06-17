042021aa795c06d41ac0ff0a83df95fc1d77c6ee
Zihao Zhao <1398687709@qq.com>
2024-06-17 12:27:20 +0800

    2024-06-17

--- src/views/DigitalObjectSearch/index.vue
+++ src/views/DigitalObjectSearch/index.vue
@@ -1,28 +1,57 @@
 <template>
     <div style="display: flex;">
         <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
-            <el-form :model="searchForm" label-width="auto" class="SearchForm">
-                <el-form-item label="DOI" class="SearchFormItem">
-                    <el-input v-model="searchForm.doi"></el-input>
+            <el-form :model="searchForm" label-width="auto" :rules="rules" class="SearchForm">
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.projectDoi" placeholder="项目DOI"></el-input>
                 </el-form-item>
-                <el-form-item label="数字对象名字" class="SearchFormItem">
-                    <el-input v-model="searchForm.doiName"></el-input>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.doi" placeholder="DOI"></el-input>
                 </el-form-item>
-                <el-form-item label="数字对象来源" class="SearchFormItem">
-                    <el-input v-model="searchForm.doiSource"></el-input>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.name" placeholder="数字对象名称"></el-input>
                 </el-form-item>
-                <el-form-item label="数字对象描述" class="SearchFormItem">
-                    <el-input v-model="searchForm.doiDesc"></el-input>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.type" placeholder="数字对象类型"></el-input>
                 </el-form-item>
-                <el-form-item label="数字对象所属项目" class="SearchFormItem">
-                    <el-input v-model="searchForm.project"></el-input>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.status" placeholder="数字对象状态"></el-input>
                 </el-form-item>
-                <el-form-item label="数字对象所属机构" class="SearchFormItem">
-                    <el-input v-model="searchForm.institution"></el-input>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.description" placeholder="数字对象描述"></el-input>
+                </el-form-item>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.source" placeholder="数字对象来源"></el-input>
+                </el-form-item>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.institutionDoi" placeholder="机构DOI"></el-input>
+                </el-form-item>
+                <el-form-item class="SearchFormItem">
+                    <el-input v-model="searchForm.institutionName" placeholder="机构名字"></el-input>
+                </el-form-item>
+                <el-form-item class="SearchFormItem">
+                    <el-date-picker
+                        v-model="searchForm.createTimeRange"
+                        type="daterange"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        value-format="yyyy-MM-dd">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item class="SearchFormItem">
+                    <el-date-picker
+                        v-model="searchForm.updateTimeRange"
+                        type="daterange"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        value-format="yyyy-MM-dd">
+                    </el-date-picker>
                 </el-form-item>
             </el-form>
 
-            <el-button type="primary">搜索</el-button>
+            <el-button type="primary" @click="searchData">搜索</el-button>
             <el-divider></el-divider>
 
             <el-table :data="resultTable" stripe border style="width: 95%;">
@@ -33,28 +62,55 @@
                 <el-table-column prop="project" label="数字对象所属项目"></el-table-column>
                 <el-table-column prop="institution" label="数字对象所属机构"></el-table-column>
             </el-table>
+
+            <div style="margin: 24px">
+                <el-pagination background layout="pager" :page-size="10" :page-count="pages"
+                    @current-change="clickPage">
+                </el-pagination>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import { postForm } from '@/api/data'
 export default {
     name: "DigitalObjectSearch",
     data() {
         return {
+            pages: 1,
+            currentPage: 1,
             searchForm: {
+                // 项目DOI
+                projectDoi: '',
                 // DOI
                 doi: '',
                 // 数字对象名称
-                doiName: '',
-                // 数字对象来源
-                doiSource: '',
+                name: '',
+                // 数字对象类型
+                type: '',
+                // 数字对象状态
+                status: '',
                 // 数字对象描述
-                doiDesc: '',
-                // doi所属项目
-                project: '',
-                // doi所属机构
-                institution: '',
+                description: '',
+                // 数字对象来源
+                source: '',
+                // 机构DOI
+                institutionDoi: '',
+                // 机构名字
+                institutionName: '',
+                // 创建时间范围
+                createTimeRange: [],
+                // 更新时间范围
+                updateTimeRange: [],
+                // 页码
+                pageNo: 1,
+            },
+
+            rules: {
+                projectDoi: [
+                    { required: false, message: '请输入项目DOI', trigger: 'blur' }
+                ]
             },
 
             resultTable: [
@@ -67,11 +123,64 @@ export default {
                     institution: '机构1',
                 }
             ],
-
         };
     },
-    mounted() {},
-    methods: {},
+    mounted() {
+        this.getData({})
+    },
+    methods: {
+        clickPage(page) {
+            this.currentPage = page;
+            this.searchForm.pageNo = this.currentPage;
+            this.getData(this.searchForm);
+        },
+        searchData() {
+            if (this.searchData.projectDoi === '') {
+                this.$message.warn('请输入项目DOI');
+                return;
+            }
+
+            let postData = {
+                projectDoi: this.searchForm.projectDoi,
+                doi: this.searchForm.doi,
+                name: this.searchForm.name,
+                type: this.searchForm.type,
+                status: this.searchForm.status,
+                description: this.searchForm.description,
+                source: this.searchForm.source,
+                institutionDoi: this.searchForm.institutionDoi,
+                institutionName: this.searchForm.institutionName,
+            };
+            if (this.searchForm.createTimeRange.length > 0) {
+                postData.createTimeStart = this.searchForm.createTimeRange[0];
+                postData.createTimeEnd = this.searchForm.createTimeRange[1];
+            }
+            if (this.searchForm.updateTimeRange.length > 0) {
+                postData.updateTimeStart = this.searchForm.updateTimeRange[0];
+                postData.updateTimeEnd = this.searchForm.updateTimeRange[1];
+            }
+        },
+        
+        getData(postData) {
+            _this.resultTable = [];
+            postData.pageSize = 10;
+            postData.pageNo = this.currentPage;
+            let _this = this;
+            postForm('/registry/searchMetaData', postData, _this, function(res){
+                _this.pages = res.data.pages;
+                for(let item of res.data.records) {
+                    _this.resultTable.push({
+                        doi: item.doi,
+                        doiName: item.name,
+                        doiSource: item.source,
+                        doiDesc: item.description,
+                        project: item.project,
+                        institution: item.institution,
+                    })
+                }
+            })
+        }
+    },
 }
 </script>
 
