74df1cdb3da44157a3638bd66203d18166eaeec9
Zihao Zhao <1398687709@qq.com>
2024-10-11 15:08:14 +0800

    2024-10-11

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -164,7 +164,7 @@ export default {
                 this.$router.push('/PermissionControl')
             }
             else if (key === '9') {
-                window.open("http://8.130.160.66:8085/")
+                window.open("http://8.130.160.66:8085/Login?insName=正大天晴")
             }
         },
     },
