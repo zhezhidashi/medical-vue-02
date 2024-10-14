d904d4b12b2ef13e6ae249be8748112626d3b91e
Zihao Zhao <1398687709@qq.com>
2024-10-14 14:41:12 +0800

    2024-10-14

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -40,7 +40,7 @@ export default {
     data() {
         return {
             isAdmin: false,
-            activeIndex: '4',
+            activeIndex: '2-2',
             path: '',
         };
     },
