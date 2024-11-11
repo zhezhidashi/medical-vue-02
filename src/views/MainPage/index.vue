2d8d5fe54f131b95105b4a24e0eef6e9a2eaa1ad
Zihao Zhao <1398687709@qq.com>
2024-11-11 14:46:20 +0800

    2024-11-11

--- src/views/MainPage/index.vue
+++ src/views/MainPage/index.vue
@@ -161,6 +161,9 @@ export default {
                         _this.initEcharts1();
                     })
                 }
+                else{
+                    _this.initEcharts1();
+                }
             })
         },
         getApproveStatus() {
